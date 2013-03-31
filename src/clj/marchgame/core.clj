3b0c6aa92f3e49a0877db1d9cfe11286ec61caa8
Eugene Tan <jmingtan@gmail.com>
2013-04-01 01:05:50 +0800

    Analytics

--- src/clj/marchgame/core.clj
+++ src/clj/marchgame/core.clj
@@ -15,6 +15,13 @@
      "/css/font-awesome.css"
      "/css/font-awesome-ie7.css"
      "/css/site.css")
+    (javascript-tag "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-37871421-3', 'eugene-tan.info');
+  ga('send', 'pageview');")
     [:title "Derelict"]]
    [:body {:onload "marchgame.core.init();"}
     [:div.container
